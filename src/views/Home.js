0d78d91c598564725a109cd6eb97ce09c033f87e
Ceo-Sammarco <noah.kd@gmail.com>
2024-04-02 13:00:36 -0300

    Ceo-Sammarco - Tue 02 Apr 2024

--- src/views/Home.js
+++ src/views/Home.js
@@ -16,9 +16,7 @@ export default function Home( props ) {
    return( <>
       <View>
          <Text>&lt;- Home -&gt;</Text>
-         {/*<Pressable onPress={ () => props.onExit( Views.About ) }>*/}
          <Pressable onPress={ () => props.onExit( Views.About ) }>
-         {/*<Pressable onPress={ () => serViewholder( Views.About ) }>*/}
             <Text>About</Text>
          </Pressable>
       </View>
